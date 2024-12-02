4b6971148b5ca955c72bd08ab154bb706b334e3c
JerryCap <160643022+JerryCap@users.noreply.github.com>
2024-12-02 14:51:09 -0500

    Create Buttons.js

---
+++ src/Buttons.js
@@ -0,0 +1,16 @@
+// JavaScript to toggle collapsible content
+var coll = document.getElementsByClassName("collapsible");
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        // Toggle the active class
+        this.classList.toggle("active");
+
+        // Get the next element and toggle display
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
